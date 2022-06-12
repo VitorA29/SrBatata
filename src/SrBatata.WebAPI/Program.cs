22f801c74d03fa26d78814c192fdeb89a3e660ad
VitorA29 <vitor_araujo.29@outlook.com>
2022-06-12 20:20:02 -0300

    Performs some minor refactoring in Program class

--- src/SrBatata.WebAPI/Program.cs
+++ src/SrBatata.WebAPI/Program.cs
@@ -1,25 +1,61 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace SrBatata.WebAPI
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = BuildWebApplication(args);
+
+            ConfigureMiddleware(app, app.Environment, app.Services);
+            ConfigureEndpoints(app, app.Services);
+
+            app.Run();
+        }
+
+        public static WebApplication BuildWebApplication(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            ConfigureConfiguration(builder.Configuration);
+            ConfigureServices(builder.Services);
+
+            return builder.Build();
+        }
+
+        private static void ConfigureConfiguration(ConfigurationManager configuration) 
+        {
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        private static void ConfigureMiddleware(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
+        {
+            // Configure the swagger.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            // Configure the HTTP request pipeline.
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+        }
+
+        private static void ConfigureEndpoints(IEndpointRouteBuilder app, IServiceProvider services)
+        {
+            app.MapControllers();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
